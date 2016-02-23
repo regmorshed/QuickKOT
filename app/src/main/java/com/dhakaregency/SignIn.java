834b5b98b4a9493b089361d3fb37dbde786ee450
Morshedur M. Rahman <m.morshed@regency.local>
2016-02-23 15:20:36 +0600

    Initial commit

---
+++ app/src/main/java/com/dhakaregency/SignIn.java
@@ -0,0 +1,24 @@
+package com.dhakaregency;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.dhakaregency.quickkot.R;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+    }
+
+
+
+
+}
