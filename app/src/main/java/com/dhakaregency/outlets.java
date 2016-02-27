e53b65dbf001cfba5d3c98e8c87bfc92c5e62f63
regmorshed <morshed@dhakaregency.com>
2016-02-27 16:16:16 +0600

    Outlets Added

--- app/src/main/java/com/dhakaregency/outlets.java
+++ app/src/main/java/com/dhakaregency/outlets.java
@@ -1,16 +1,37 @@
 package com.dhakaregency;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import com.dhakaregency.quickkot.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class outlets extends AppCompatActivity {
 
+    public   String muserId="";
+    public String moduleid="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +39,187 @@ public class outlets extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Bundle b = getIntent().getExtras();
+        muserId= b.getString("userid");
+
+        ArrayList<String> passing = new ArrayList<String>();
+        passing.add(muserId);
+        passing.add(muserId);
+        LoadOutlets loadOutlets = new LoadOutlets();
+        loadOutlets.execute(passing);
     }
 
+    public void PopulateOutlets(ArrayList<OutletsEntity> outletsEntityArrayList) {
+        boolean isFirstTime = true;
+        boolean isColumnCountingFinished = false;
+        TableLayout tableLayout = (TableLayout) findViewById(R.id.outletsTableLayout);
+        TableRow tableRow = null;
+        int col = 0;
+        for (final OutletsEntity outletsEntity : outletsEntityArrayList) {
+            if (isFirstTime) {
+                tableRow = new TableRow(getApplicationContext());
+                tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                        TableLayout.LayoutParams.MATCH_PARENT,
+                        TableLayout.LayoutParams.MATCH_PARENT, 10f
+                ));
+                tableLayout.addView(tableRow);
+                isFirstTime = false;
+            }
+            if (!isFirstTime && isColumnCountingFinished){
+                tableRow = new TableRow(getApplicationContext());
+                tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                        TableLayout.LayoutParams.MATCH_PARENT,
+                        TableLayout.LayoutParams.MATCH_PARENT, 1.0f
+                ));
+                tableLayout.addView(tableRow);
+                isColumnCountingFinished = false;
+                col = 0;
+            }
+            if (!isColumnCountingFinished) {
+
+                Button button = new Button(getApplicationContext());
+                button.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT, 1.0f
+                ));
+                moduleid=outletsEntity.getId().toString();
+                button.setText(outletsEntity.getName().toString());
+                button.setPadding(0, 0, 0, 0);
+
+                button.setOnClickListener(new View.OnClickListener() {
+                                              @Override
+                                              public void onClick(View v) {
+
+                                                  GotoGuestTypeChoise(muserId, moduleid);
+                                              }
+                                          }
+                );
+                tableRow.addView(button);
+            }
+            if (col > 2) {
+                isColumnCountingFinished = true;
+            }
+            else {
+                col++;
+            }
+
+        }
+
+    }
+
+    private void GotoGuestTypeChoise(String userId,String moduleId)
+    {
+        
+
+    }
+
+    public class LoadOutlets extends AsyncTask<ArrayList<String>, Void, ArrayList<OutletsEntity>> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<OutletsEntity> outletsEntityArrayList) {
+            super.onPostExecute(outletsEntityArrayList);
+            PopulateOutlets(outletsEntityArrayList);
+        }
+
+        @Override
+        protected ArrayList<OutletsEntity> doInBackground(ArrayList<String>... params) {
+
+            String str = "http://192.168.99.12:8080/AuthService.svc/GetModules";
+            String response = "";
+            ArrayList<OutletsEntity> outletsEntities = new ArrayList<>();
+
+            URL url = null;
+            try {
+                url = new URL(str);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            try {
+
+                HttpURLConnection conn = null;
+                try {
+                    conn = (HttpURLConnection) url.openConnection();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                ArrayList<String> passed = params[0];
+
+                conn.setReadTimeout(15000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                conn.setRequestProperty("Content-Type", "application/json");
+
+
+                JSONObject jsonObject = new JSONObject();
+                // Build JSON string
+                JSONStringer userJson = new JSONStringer()
+                        .object()
+                        .key("user")
+                        .object()
+                        .key("userid").value(passed.get(0).toString())
+                        .key("password").value(passed.get(1).toString())
+                        .endObject()
+                        .endObject();
+
+                //byte[] outputBytes = jsonParam.toString().getBytes("UTF-8");
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(conn.getOutputStream());
+                outputStreamWriter.write(userJson.toString());
+                outputStreamWriter.close();
+
+                int responseCode = conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    String line;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    while ((line = br.readLine()) != null) {
+                        response += line;
+                    }
+                } else {
+                    response = "";
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            Integer result = 0;
+            JSONObject jObject = null;
+            if (!response.isEmpty()) {
+                try {
+                    jObject = new JSONObject(response);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            try {
+                JSONArray jsonArray = (JSONArray) jObject.getJSONArray("GetModulesResult");
+                try {
+
+                    for (int i=0;i<jsonArray.length();i++) {
+                        JSONObject object = jsonArray.getJSONObject(i);
+                        OutletsEntity outletsEntity = new OutletsEntity();
+                        outletsEntity .setId(object.getString("code"));
+                        outletsEntity.setName(object.getString("description"));
+                        outletsEntities.add(outletsEntity);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return outletsEntities;
+        }
+    }
 }
