fd672f4b61fa715b6793307d0b8ff97406886f23
regsohan <shohan@dhakaregency.com>
2016-02-28 17:55:04 +0600

    Merge branch 'TestDeploy' of https://github.com/regAdmin/QuickKOT into TestDeploy
    # Conflicts:
    #	app/src/main/res/layout/activity_kot.xml

--- app/src/main/java/com/dhakaregency/KOT.java
+++ app/src/main/java/com/dhakaregency/KOT.java
@@ -1,15 +1,21 @@
 package com.dhakaregency;
-
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
 import com.dhakaregency.quickkot.R;
 
-public class KOT extends AppCompatActivity {
+public class KOT extends FragmentActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kot);
+        // kot updated
     }
 }
