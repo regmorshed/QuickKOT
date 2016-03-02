a42f6f34ec1a8d3a11ed0ee994dc020e156c9225
regmorshed <morshed@dhakaregency.com>
2016-03-02 18:40:33 +0600

    sub menu

--- app/src/main/java/com/dhakaregency/OnFoodTypeSelectedInterface.java
+++
@@ -1,8 +0,0 @@
-package com.dhakaregency;
-
-/**
- * Created by Administrator on 02/03/2016.
- */
-public interface OnFoodTypeSelectedInterface {
-    public void onFoodTypeSelection(String moduleid,String foodtype);
-}
---
+++ app/src/main/java/com/dhakaregency/SubMenuList.java
@@ -0,0 +1,26 @@
+package com.dhakaregency;
+
+/**
+ * Created by Administrator on 02/03/2016.
+ */
+public class SubMenuList {
+
+    private  String code;
+    private String description;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
